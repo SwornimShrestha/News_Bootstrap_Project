3e9d8c7887111f14a51a9d0901c7e33ce7abd2c7
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-03-23 09:51:29 +0545

    first commit

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -4,7 +4,9 @@ const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=ec783a9aafd84d83962f7ca40891d7c4`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setArticles(data.articles));
